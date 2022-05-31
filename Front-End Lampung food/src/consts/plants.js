121494395dd56dc3f47a3b51037f5c30a7e8198b
adamrahmaan17 <74776786+adamrahmaan17@users.noreply.github.com>
2022-05-31 22:16:02 +0700

    Add files via upload

---
+++ Front-End Lampung food/src/consts/plants.js
@@ -0,0 +1,80 @@
+const plants = [
+  {
+    id: 1,
+    name: 'Sambal Lampung',
+    price: '25k',
+    like: true,
+    img: require('../assets/plant1.png'),
+    about:
+      'Bagi pencinta pedas, oleh-oleh khas yang satu ini wajib dibawa pulang saat berkunjung ke LampungFood. Sambal ini terdiri dari berbagai pilihan yang menarik, mulai dari sambal goreng udang, sambal cuka, sambal ijo Lampung, sambal bawang Lampung, sambal ikan peda, serta sambal tempoyak dan seruit yang tak kalah lezat. ',
+  },
+
+  {
+    id: 2,
+    name: 'Kemplang',
+    price: '15k',
+    like: false,
+    img: require('../assets/plant2.png'),
+    about:
+      'Serupa dengan kerupuk ikan asal Palembang bernama amplang, oleh-oleh khas Lampung ini juga berbahan dasar ikan tenggiri. Bedanya, makanan renyah ini disebut dengan nama kemplang. Uniknya, proses pembuatan kemplang tidak digoreng dengan minyak, melainkan menggunakan pasir.',
+  },
+  {
+    id: 3,
+    name: 'Keripik Pisang',
+    price: '20k',
+    like: false,
+    img: require('../assets/plant3.png'),
+    about:
+      'Lampung dikenal sebagai salah satu daerah dengan penghasil pisang terbesar di Indonesia. Maka dari itu, LampungFood memproduksi olahan keripik pisang yang memiliki ciri khas pada rasa dan tekstur. Pisang yang diproduksi menggunakan pisang ambon ini memiliki berbagai varian rasa, mulai dari cokelat, keju, durian, stroberi, hingga balado',
+  },
+
+  {
+    id: 4,
+    name: 'Potted Plant',
+    price: '10k',
+    like: true,
+    img: require('../assets/plant4.png'),
+    about:
+      'Selain keripik pisang, LampungFood juga mengolah pisang menjadi pie yang memiliki bentuk garing di luar dan lembut di dalam dengan berbagai isian.',
+  },
+  {
+    id: 5,
+    name: 'Lapis Legit',
+    price: '300K',
+    like: true,
+    img: require('../assets/plant5.png'),
+    about:
+      'Di LampungFood, kue lapis legit yang diproduksi dengan menggunakan 21 butir telur dalam satu adonan saat proses pembuatan, sehingga memiliki tekstur yang lembut',
+  },
+  {
+    id: 6,
+    name: 'Manisan',
+    price: '35k',
+    like: false,
+    img: require('../assets/plant6.png'),
+    about:
+      'Manisan juga menjadi salah satu oleh-oleh khas Lampung yang sayang untuk dilewatkan begitu saja. Di LampungFood, terdapat banyak manisan buah-buahan yang telah melalui proses pengawetan dengan direndam bersama air gula selama beberapa waktu.',
+  },
+  {
+    id: 7,
+    name: 'Lempuk',
+    price: '35k',
+    like: false,
+    img: require('../assets/plant7.png'),
+    about:
+      'Di Pulau Jawa, lempuk disebut juga dengan istilah dodol atau jenang karena proses pembuatan keduanya yang sama persis. Di LampungFood, lempuk menjadi salah satu oleh-oleh populer yang bisa dibawa pulang. ',
+  },
+
+  {
+    id: 8,
+    name: 'Pempek',
+    price: '100k',
+    like: false,
+    img: require('../assets/plant8.png'),
+    about:
+      'Pempek khas LampungFood ini berbeda dengan pempek Palembang yang biasa dijumpai di pasaran. Pasalnya, pempek ini diproduksi menggunakan ikan tenggiri, sedangkan yang banyak dijumpai dipasaran menggunakan bahan dasar ikan belida.',
+  },
+
+];
+
+export default plants;
